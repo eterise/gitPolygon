fedd18405d45edae849975e726cd419553a18a8b
eterise <a-ifs88_stas@mail.ru>
2022-12-28 23:21:57 +0300

    Calc was updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const calc = (a, b, action) => {
+  if (typeof a != "number" || typeof b != "number") return;
+
   switch (action) {
     case "Добавить":
       return a + b;
     case "Вычесть":
       return a - b < 0 ? 0 : a - b;
+    case "Умножить":
+      return a * b;
   }
 };
